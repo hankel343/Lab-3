daa583e7ffa9548927c712ca49437d66afba28f6
hankel343 <57358008+hankel343@users.noreply.github.com>
2020-09-20 20:30:15 -0500

    1.) Resolved run-time error that would return standard deviation as a rounded float.
    2.) Built the file stream portion along with input and output files

--- Lab3/Source.cpp
+++ Lab3/Source.cpp
@@ -7,23 +7,18 @@
 using namespace std;
 
 //Function prototypes for Mean_Calc and StdDev_Calc
-int Mean_Calc();
-int StdDev_Calc();
-
-//identifiers
-int num1;
-int num2;
-int num3;
-int num4;
-
-
+int Mean_Calc(int num1, int num2, int num3, int num4);
+float StdDev_Calc(float num1, float num2, float num3, float num4, float mean);
 
 int main()
 {
 	//Input identifiers
-	
-	ifstream inFile;
-	ofstream outFile;
+	ifstream inFile; // Input stream for files
+	ofstream outFile; // Output stream for files
+	int num1;
+	int num2;
+	int num3;
+	int num4;
 
 	char ans;
 	cout << "Do you have a file you would like input from ? Y/N" << endl;
@@ -33,50 +28,52 @@ int main()
 		inFile.open("inMeanStd.dat.txt");
 		outFile.open("outMeanStd.dat.txt");
 		inFile >> num1 >> num2 >> num3 >> num4; //Reads integers from inMeanStd.dat.txt into num input variables
-		cout << "The mean of these four integers is: " << endl;
-		outFile << fixed << showpoint << setprecision(2) 
-			<< float(Mean_Calc()); //Type casts the return value as a float
-		cout << "The standard deviation of these four integers is: " << endl;
+		
+		outFile << "The mean of these four integers is: " << endl;
+		outFile << Mean_Calc(num1, num2, num3, num4) << endl;
+
+		float mean = Mean_Calc(num1, num2, num3, num4);
+
+		outFile << "The standard deviation of these four integers is: " << endl;
+		outFile << fixed << setprecision(6)
+			<< float(StdDev_Calc(num1, num2, num3, num4, mean));
 
 	} 
 	else
 	{
 		cout << "Enter four integers: " << endl;
 		cin >> num1 >> num2 >> num3 >> num4; //Reads values into num variables from keyboard
-		
-		Mean_Calc();
-		int mean = Mean_Calc();
 
 		cout << "The mean of these four integers is: " << endl;
-		cout << fixed << showpoint << setprecision(2)
-			<< float(Mean_Calc()) << endl;;
+		cout << Mean_Calc(num1, num2, num3, num4) << endl;
 
-		StdDev_Calc();
+		/*This assignment statement sets the value of the identifer mean to the return value of Mean_Calc,
+		  which is the mean of the four integers passed to the function.*/
+		float mean = Mean_Calc(num1, num2, num3, num4);
 
 		cout << "The standard deviation of these four integers is: " << endl;
-		cout << fixed << showpoint <<
-			float(StdDev_Calc());
+		cout << fixed << setprecision(6)
+			<< float(StdDev_Calc(num1, num2, num3, num4, mean)); /*********************************************************************************
+																  *Standard deviation function is passed the four integers along with their mean. *
+																  *The return value is the standard deviation of the four integers,				  *
+																  *and this is what is printed to the console via the output stream.			  *
+																  *********************************************************************************/
 	}
 
 	return 0;
 }
 
 //Mean_Calc definition: Calculates the mean of the four integers passed
-int Mean_Calc()
+int Mean_Calc(int num1, int num2, int num3, int num4)
 {
-	int mean;
-	int sum = num1 + num2 + num3 + num4;
-	mean = sum / 4;
-
+	int mean = (num1 + num2 + num3 + num4) / 4;
 	return mean;
 }
 
 //StdDev_Calc definition: Calculates standard deviation of four integers passed
-int StdDev_Calc()
+float StdDev_Calc(float num1, float num2, float num3, float num4, float mean)
 {
-	float sum = num1 + num2 + num3 + num4;
-	float mean = sum / float(4);
-	float StdDev = sqrt(((pow(num1 - mean, 2) + pow(num2 - mean, 2) + pow(num3 - mean, 2) + pow(num4 - mean, 2)) / 4));
+	float StdDev = float(sqrt(((pow(num1 - mean, 2) + pow(num2 - mean, 2) + pow(num3 - mean, 2) + pow(num4 - mean, 2)) / 4)));
 	
 	return StdDev;
 }
